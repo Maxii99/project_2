fb004dd9c1aa2b176f54ddf86668555a59982abe
Said Pakyari <spakyai.ds@gmail.com>
2020-10-11 11:30:00 -0700

    PG database is ready. CSV's updated

--- Flask_app/ETL.py
+++ Flask_app/ETL.py
@@ -1,72 +1,25 @@
+import pandas as pd
 import numpy as np
-import datetime as dt
+import sqlite3
+from sqlalchemy import create_engine
 
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+# Import Migration Map Data table.
+csv_file = "CSV/Migration_Map_Data.csv"
+migration_df = pd.read_csv(csv_file)
+cnx = sqlite3.connect('Database/db_migration.sqlite')
+migration_df.to_sql('Migration_Data', cnx)
 
-from flask import Flask, jsonify
+# Import Immigration to and from 2007 to 2018 table.
+csv_file = "CSV/Immigration to and from 2007 to 2018.csv"
+Immigration_df = pd.read_csv(csv_file)
+cnx = sqlite3.connect('Database/db_migration.sqlite')
+Immigration_df.to_sql('Immigration_details', cnx)
 
+# Import Regions table.
+csv_file = "CSV/Regions.csv"
+Regions_df = pd.read_csv(csv_file)
+Regions_df.head()
+cnx = sqlite3.connect('Database/db_migration.sqlite')
+Regions_df.to_sql('Regions', cnx)
 
-#################################################
-# Database Setup
-#################################################
-engine = create_engine( "sqlite:///C:\\db\\hawaii.sqlite")
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-# Save reference to the table
-measurments = Base.classes.measurement
-station = Base.classes.station
-
-#################################################
-# Flask Setup
-#################################################
-app = Flask(__name__)
-
-
-#################################################
-# Flask Routes
-#################################################
-
-@app.route("/")
-def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/(start)<br/>"
-        f"/api/v1.0/(start)/(end)<br/>"
-    )
-
-
-@app.route("/api/v1.0/precipitation")
-def prcps():
-
-    # Convert the query results to a dictionary using date as the key and prcp as the value.
-
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-   
-    # Query all dates and paticipations
-    results = session.query(measurments.date, measurments.prcp).all()
-
-    session.close()
-
-    # Create a dictionary of the dates and participations
-    prcps = dict((date, prcp) for date, prcp in results)
-
-    return jsonify(prcps)
-
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+cnx.close()
