0121176128ce0d1c67725b38f4e06d0b3460c230
Said Pakyari <spakyai.ds@gmail.com>
2020-10-12 02:09:45 -0700

    Demography API ver.1 ready

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,5 +1,7 @@
 import numpy as np
 import datetime as dt
+import pandas as pd
+import os
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -12,7 +14,8 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine( "sqlite:///C:\\db\\hawaii.sqlite")
+rds_connection_string = "immigration_cnn:@localhost:5432/migration_db"
+engine = create_engine(f'postgresql://{rds_connection_string}')
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -20,8 +23,11 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 # Save reference to the table
-measurments = Base.classes.measurement
-station = Base.classes.station
+details = Base.classes.details
+regional = Base.classes.regional
+regions = Base.classes.regions
+
+labels =['Age 0-5', 'Age 6-10', 'Age 11-15', 'Age 16-20', 'Age 20+', '9th Grade', '12th Grade', 'HS_Dropout', 'HS Grad', 'College 2 Yr', 'Bach Degree', 'Adv Degree', 'College Enroll 18-24', 'College Enroll 25+', 'Median Income FT', 'Median Income PT', 'Income_0_25k', 'Income_25k_48k', 'Income_48k_77k', 'Income_77k_125k', 'Income_125k+', 'Management', 'Science & Eng.', 'Legal Social Service', 'Education & Arts', 'Health care', 'Food prep serve', 'Cleaning & maint.', 'Other services', 'Sales', 'Administrative', 'Farming & fishing', 'Construction', 'Manufacturing', 'Transportation', 'Military', 'Unemployed']
 
 #################################################
 # Flask Setup
@@ -38,32 +44,50 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/(start)<br/>"
-        f"/api/v1.0/(start)/(end)<br/>"
+        f"/api/v1.0/migration_data/(demography)[age\education\median_income\income\occupation]<br/>"
     )
 
 
-@app.route("/api/v1.0/precipitation")
-def prcps():
+@app.route("/api/v1.0/migration_data/<demography>")
+def migration_data(demography):
 
-    # Convert the query results to a dictionary using date as the key and prcp as the value.
+    # csvpath = os.path.join('.', 'csv', 'labels.csv')
 
-    # Create our session (link) from Python to the DB
+    # labels = pd.read_csv(csvpath)
+
     session = Session(engine)
+    stmt = session.query(regional).statement
+    df = pd.read_sql_query(stmt, session.bind)
+    session.close
 
-   
-    # Query all dates and paticipations
-    results = session.query(measurments.date, measurments.prcp).all()
+    meta_dict = {'demography':demography}
 
-    session.close()
+    if demography == 'age':
+    
+        demography_df = df.iloc[:,1:7].T
+        meta_dict['labels'] =  labels[0:6]
+    
+    elif demography == 'education':
+        demography_df = df.iloc[:,np.r_[1, 7:16]].T
+        meta_dict['labels'] =  labels[6:15]
+        
+    elif demography == 'median_income':
+        demography_df = df.iloc[:,np.r_[1, 16:18]].T
+        meta_dict['labels'] =  labels[15:17]
 
-    # Create a dictionary of the dates and participations
-    prcps = dict((date, prcp) for date, prcp in results)
+    elif demography == 'income':
+        demography_df = df.iloc[:,np.r_[1, 18:23]].T
+        meta_dict['labels'] =  labels[17:22]
+        
+    elif demography == 'occupation':
+        demography_df = df.iloc[:,np.r_[1, 23:]].T
+        meta_dict['labels'] =  labels[22:]       
+    
 
-    return jsonify(prcps)
+    traces = {demography_df[i].to_list()[0] : demography_df[i].to_list()[1:] for i in demography_df}
+
+    myJSON = [meta_dict, traces]
+    return jsonify(myJSON)
 
 
 
